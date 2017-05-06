657a56e2e0afc7e5ed9f3b6b13004c0b3b71a8a4
chiguozi <qdlzr456@163.com>
2017-05-06 18:32:15 +0800

    commit

---
+++ Assets/Editor/AnimationPlayer/AnimationClipControl.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class AnimationClipControl
+{
+    AnimationClip _clip;
+    float _length;
+    public float length { get { return _length; } }
+    public bool loop = false;
+
+    GameObject _ownerGo;
+    bool _play = false;
+    float _startTime;
+    float _escapeTime;
+    public static void Init()
+    {
+        AnimationMode.StartAnimationMode();
+    }
+
+    public void SetOwnGo(GameObject go)
+    {
+        _ownerGo = go;
+    }
+
+    public void SetClip(AnimationClip clip)
+    {
+        _clip = clip;
+        if (clip == null)
+        {
+            _length = 0;
+        }
+        else
+        {
+            _length = clip.length;
+        }
+    }
+
+    public void Play()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Reset()
+    {
+
+    }
+
+}
